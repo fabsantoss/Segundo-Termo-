44628222949c08abfb201aebfea9164110d66eef
fabsantoss <fabianesantossantos954@gmail.com>
2020-03-03 14:04:08 -0300

    Exercicio 03/-3

--- InLock_Game_Tarde-03-DQL.sql
+++ InLock_Game_Tarde-03-DQL.sql
@@ -19,5 +19,16 @@ LEFT JOIN Jogos on Estudios.IdEstudio = Jogos.IdJogo
 SELECT U.IdUsuario, U.Email, U.IdTipoUsuario, TU.Tipo FROM Usuarios U
 INNER JOIN TiposUsuario TU
 ON U.IdTipoUsuario = TU.IdTipoUsuario
-WHERE U.Email = 'cliente@cliente.com' AND U.Senha = 'cliente'
+WHERE U.Email = 'cliente@cliente.com' AND U.Senha = 'cliente' 
 
+
+SELECT   Jogos.IdJogo,  Jogos.NomeJogo , Jogos.Descricao, Jogos.DataLancamento, Jogos.Valor, Estudios.NomeEstudio
+FROM Jogos 
+INNER JOIN Estudios on Jogos.IdJogo = Estudios.IdEstudio 
+WHERE Jogos.IdJogo = '1' ;
+
+
+SELECT   Estudios.IdEstudio,  Estudios.NomeEstudio,Jogos.NomeJogo, Jogos.Descricao, Jogos.DataLancamento, Jogos.Valor
+FROM Estudios 
+LEFT JOIN Jogos on Estudios.IdEstudio = Jogos.IdJogo
+WHERE Estudios.IdEstudio = '2' ;
